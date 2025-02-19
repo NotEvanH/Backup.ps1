218ae0965458f9d91dd0afc506b772ad539cafa0
NotEvanH <158800060+NotEvanH@users.noreply.github.com>
2025-02-19 18:10:20 +0800

    Create test.ps1
    sdhglksdjghsdg

---
+++ test.ps1
@@ -0,0 +1,15 @@
+# I don't even know tbh
+
+If (1 -eq 1) {
+    Write-Host "One is equal to one"
+}
+
+$fruits = @("apple", "cheese", "robot")
+$fruits[0]
+
+$i = 0
+
+while ($i -lt 5) {
+    Write-Host "Iteration: $i"
+    $i++
+}
